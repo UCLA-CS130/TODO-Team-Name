30a55377948a741a4e9649c789154c705a9b1789
Matt Dragotto <matthewdragotto@gmail.com>
2017-02-07 08:16:46 +0000

    -Added files mistakenly not added in last commit TODO: Print out better messages to terminal, error handling in static _request_handler

---
+++ src/server_options.hpp
@@ -0,0 +1,32 @@
+//
+// request.hpp
+// ~~~~~~~~~~~
+//
+// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef SERVER_OPTIONS_HPP
+#define SERVER_OPTIONS_HPP
+
+#include <string>
+#include <map>
+
+namespace http {
+namespace server {
+
+// holds the parsed config server options
+struct server_options {
+  std::string port;
+  std::string echo_handler;
+
+  //Key of the map is the path in url, value is the root on localhost
+  std::map<std::string, std::string> static_files_map;
+};
+
+} // namespace server
+} // namespace http
+
+#endif // SERVER_OPTIONS_HPP
