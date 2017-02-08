f7e16f2a018bcac6ce2de6ac2d4ec377864b284f
McKenna Galvin <mckennagalvin@me.com>
2017-02-07 18:38:13 -0800

    Improve coverage of request_parser unit tests

--- test/request_parser_test.cpp
+++ test/request_parser_test.cpp
@@ -7,12 +7,15 @@
 
 class RequestParserTest : public ::testing::Test {
 protected:
-	boost::tribool ParseRequest(std::string http_request) {
+	bool ParseRequest(std::string http_request) {
 		char * begin = strdup(http_request.c_str());
 		char * end = begin + http_request.length();
 		boost::tribool result;
 		boost::tie(result, boost::tuples::ignore) = request_parser_.parse(request_, begin, end);
-		return result;
+		if (result) {
+			return true;
+		}
+		return false; // Treat indeterminate and false results as bad for testing purposes
 	}
 	http::server::request_parser request_parser_;
 	http::server::request request_;
@@ -21,4 +24,40 @@ protected:
 TEST_F(RequestParserTest, GoodRequest) {
 	std::string good_request = "GET / HTTP/1.1\r\nContent-Type: text/plain\r\n\r\n";
 	EXPECT_EQ(ParseRequest(good_request), true);
+}
+
+TEST_F(RequestParserTest, AllowContentAfterHeaders) {
+	EXPECT_EQ(ParseRequest("GET / HTTP/1.1\r\nContent-Type: text/plain\r\n\r\nTEST"), true);
+}
+
+TEST_F(RequestParserTest, EmptyRequest) {
+	EXPECT_EQ(ParseRequest(""), false);
+}
+
+TEST_F(RequestParserTest, BadBeginning) {
+	EXPECT_EQ(ParseRequest("1"), false);
+}
+
+TEST_F(RequestParserTest, IncompleteRequest) {
+	EXPECT_EQ(ParseRequest("GET"), false);
+}
+
+TEST_F(RequestParserTest, NoHTTPVersion) {
+	EXPECT_EQ(ParseRequest("GET /"), false);
+}
+
+TEST_F(RequestParserTest, BadHTTPVersion) {
+	EXPECT_EQ(ParseRequest("GET / HTTP/.."), false);
+}
+
+TEST_F(RequestParserTest, NoFileRequested) {
+	EXPECT_EQ(ParseRequest("GET HTTP/1.1"), false);
+}
+
+TEST_F(RequestParserTest, IncompleteHeader) {
+	EXPECT_EQ(ParseRequest("GET / HTTP/1.1\r\nContent-Type:\r\n\r\n"), false);
+}
+
+TEST_F(RequestParserTest, NoCarriageReturnAtEnd) {
+	EXPECT_EQ(ParseRequest("GET / HTTP/1.1\r\nContent-Type: text/plain"), false);
 }
