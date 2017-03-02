17f0fa5b33fb5a48f438cefa8be9cf145ac59db7
Justin <jteo1@yahoo.com>
2017-03-01 23:26:24 -0800

    small unit test fix

--- test/http_client_test.cpp
+++ test/http_client_test.cpp
@@ -9,10 +9,8 @@
 TEST(HttpClientTest, EstablishConnectionTest) {
 	http::server::HttpClient c;
 	EXPECT_TRUE(c.EstablishConnection("www.google.com", "http"));
-	EXPECT_TRUE(c.EstablishConnection("www.google.com"));
 	EXPECT_ANY_THROW(c.EstablishConnection("www....google.com", "http"));
 	EXPECT_ANY_THROW(c.EstablishConnection("www.google.com", "f"));
-	EXPECT_ANY_THROW(c.EstablishConnection("http://www.google.com"));
 }
 
 //basic request send
@@ -22,5 +20,5 @@ TEST(HttpClientTest, SendRequestTest) {
 	ASSERT_NE(req.get(), nullptr);
 	ASSERT_TRUE(c.EstablishConnection("www.example.com", "http"));
 	auto res = c.SendRequest(*req.get());
-	EXPECT_NE(res, nullptr); // Got a response?
+	EXPECT_NE(res, nullptr);
 }
