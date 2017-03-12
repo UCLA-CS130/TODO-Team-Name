aab9166d373c175d358010f39d6d4c9c0df1970e
McKenna Galvin <mckennagalvin@me.com>
2017-03-12 15:27:41 -0700

    Added unit test for markdown rendering

--- test/request_handler_static_test.cpp
+++ test/request_handler_static_test.cpp
@@ -34,7 +34,7 @@ protected:
 };
 
 TEST_F(StaticHandlerTest, SimpleRequest) {
-	HandleStaticRequest("static1", "pics", "index.html");
+	HandleStaticRequest("static1", "deploy/www", "index.html");
 
 	std::string expected_html = "<!DOCTYPE html>\n"
 		"<html>\n<body>\n\n<h1>This is default.html</h1>\n\n"
@@ -43,6 +43,30 @@ TEST_F(StaticHandlerTest, SimpleRequest) {
 
 	std::string expected_response = "HTTP/1.1 200 OK\r\n"
 									"Content-Length: 141\r\n"
+									"Content-Type: text/html\r\n\r\n" +
+									expected_html;
+
+	EXPECT_EQ(resp.ToString(), expected_response);
+}
+
+TEST_F(StaticHandlerTest, MarkdownRequest) {
+	HandleStaticRequest("static1", "deploy/www", "markdown_test.md");
+
+	std::string expected_html = "<h1>This is a Markdown file!</h1>\n"
+	"<p>This is a <a href=\"http://www.google.com\">link</a>! "
+	"Words can be <em>italic</em>, <strong>bold</strong>, or <code>code</code>.</p>\n\n"
+	"<h2>This an h2 header</h2>\n"
+	"<p>Markdown also supports bulleted lists:</p>\n\n\n"
+	"<ul>\n"
+	"<li>This is something interesting</li>\n"
+	"<li>This is something else that is interesting</li>\n"
+	"</ul>\n\n"
+	"<hr/>"
+	"<h3>This is an h3 header</h3>\n"
+	"<p>Lorem ipsum</p>\n\n";
+
+	std::string expected_response = "HTTP/1.1 200 OK\r\n"
+									"Content-Length: 402\r\n"
 									"Content-Type: text/html\r\n\r\n" +
 									expected_html;
 
