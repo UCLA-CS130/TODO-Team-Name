3f3cf66308f6331b6173198e9bf1db179a6d31c4
mckennagalvin <mckennagalvin@me.com>
2017-03-13 06:58:01 -0700

    Fixed formatting / variable naming

--- src/sql_engine.cpp
+++ src/sql_engine.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "sql_engine.hpp"
-
 #include <stdlib.h>
 #include <string>
 #include "mysql_connection.h"
-
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
@@ -12,51 +10,50 @@
 #include <resultset_metadata.h>
 
  
-std::string SqlEngine::HandleRequest(std::string& field, int mode)
-{
-  std::string outString = "";
+std::string SqlEngine::HandleRequest(std::string& field, int mode) {
+  std::string out_string = "";
   try {
     sql::Driver *driver;
     sql::Connection *con;
     sql::Statement *stmt;
     sql::ResultSet *res;
 
-    /* Create a connection */
+    // Create a connection.
     driver = get_driver_instance();
     con = driver->connect("54.190.63.110:6603", "root", "");
-    /* Connect to the MySQL test database */
+    // Connect to the MySQL test database.
     con->setSchema("DB");
 
-    //If its a query, we want to return some results
+    // If it's a query, we want to return some results
     std::cout << "QueryStatement: " << field << "\n"; 
     stmt = con->createStatement();
-    if (mode == 1){   //query mode
+    if (mode == 1){   // query mode
       res = stmt->executeQuery(field);
-      //get result set metadata
+      // Get result set metadata
       sql::ResultSetMetaData *res_meta = res -> getMetaData();
       int columns = res_meta -> getColumnCount();
 
-      //Loop for each row
+      // Loop for each row.
       while (res->next()) {
-        /* Access column data by index, 1-indexed*/
+        // Access column data by index, 1-indexed
         for (int i = 1; i <= columns; i++) {
-          outString += res->getString(i) + ", ";
+          out_string += res->getString(i) + ", ";
         }
       }
       delete res;
     }
-    //if its an update, we dont need to return any results
-    else if (mode == 2){  //update mode
+    // If it's an update, we don't need to return any results.
+    else if (mode == 2){  // update mode
       stmt->execute(field);
-      outString = "Update Success!";
+      out_string = "Update Success!";
     }
 
     delete stmt;
     delete con;
 
   } catch (sql::SQLException &e) {
-    outString = "MYSQL ERR: INVALID SYNTAX";
+    out_string = "MYSQL ERR: INVALID SYNTAX";
   }
 
-  return outString;
+  return out_string;
 }
