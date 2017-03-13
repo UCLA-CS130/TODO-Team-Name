ba10b1473c6f822e3f42b03775e30dd11ac7b89b
mckennagalvin <mckennagalvin@me.com>
2017-03-12 23:08:14 -0700

    Added CSS for SQL interface; fixed formatting / adhering to naming convention

--- src/request_handler_sql.cpp
+++ src/request_handler_sql.cpp
@@ -22,119 +22,115 @@ namespace http {
 namespace server {
 
 RequestHandler::Status SqlHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
-	// Get root directory from config.
-	  std::vector<std::shared_ptr<NginxConfigStatement>> statements = config.statements_;
-	  if (statements.size() == 1) {
-	    std::vector<std::string> statement_tokens = statements.at(0)->tokens_;
-	    if (statement_tokens.size() == 2 && statement_tokens.at(0) == "root") {
-	      root_ = statement_tokens.at(1);
-	      return RequestHandler::OK;
-	    }
-	  }
-
-  	  return RequestHandler::BAD_CONFIG;
+    // Get root directory from config.
+    std::vector<std::shared_ptr<NginxConfigStatement>> statements = config.statements_;
+    if (statements.size() == 1) {
+        std::vector<std::string> statement_tokens = statements.at(0)->tokens_;
+        if (statement_tokens.size() == 2 && statement_tokens.at(0) == "root") {
+            root_ = statement_tokens.at(1);
+            return RequestHandler::OK;
+        }
+    }
+    return RequestHandler::BAD_CONFIG;
 }
 
 RequestHandler::Status SqlHandler::HandleRequest(const Request& request, Response* response) {
-	  //Flag for whether a query, insert, etc.
-	  int SQL_MODE = 0; //0 = none
-	  					//1 = query
-	  					//2 = update
+    // Flag for whether a query, insert, etc.
+    int SQL_MODE = 0; // 0 = none
+                      // 1 = query
+                      // 2 = update
 
-	  //Will be present in the header whenever a query is made
-	  std::string queryRequest = "query?q=";
-	  std::string updateRequest = "update?q=";
+    // Will be present in the header whenever a query is made.
+    std::string query_request = "query?q=";
+    std::string update_request = "update?q=";
 
-	  //parse request url
-	  std::string request_string = request.ToString();
-	  std::string qField = "";
+    // Parse request url.
+    std::string request_string = request.ToString();
+    std::string q_field = "";
 
-	  request_string = request_string.substr(0, request_string.find("HTTP/1.1"));
-	  //check for query
-	  std::cout << "request_string: " << request_string << "\n";
-	  std::size_t foundQ = request_string.find(queryRequest);
-	  if (foundQ != std::string::npos){
-	  	std::string request_substr = request_string.substr(request_string.find(queryRequest) + queryRequest.length());
-	  	std::string qField_d = request_substr.substr(0, request_substr.find(" ", 0));
-	  	url_decode(qField_d, qField);
-	  	SQL_MODE = 1;
-	  }
+    // Check for query.
+    std::size_t found_query = request_string.find(query_request);
+    if (found_query != std::string::npos) {
+        std::string request_substr = request_string.substr(request_string.find(query_request) + query_request.length());
+        std::string q_field_d = request_substr.substr(0, request_substr.find(" ", 0));
+        url_decode(q_field_d, q_field);
+        SQL_MODE = 1;
+    }
 
-	  //check for update
-	  std::size_t foundUp = request_string.find(updateRequest);
-	  if (foundUp != std::string::npos){
-	  	std::string request_substr = request_string.substr(request_string.find(updateRequest) + updateRequest.length());
-	  	std::string qField_d = request_substr.substr(0, request_substr.find(" ", 0));
-	  	url_decode(qField_d, qField);
-	  	SQL_MODE = 2;
-	  }
+    // Check for update.
+    std::size_t found_update = request_string.find(update_request);
+    if (found_update != std::string::npos){
+        std::string request_substr = request_string.substr(request_string.find(update_request) + update_request.length());
+        std::string q_field_d = request_substr.substr(0, request_substr.find(" ", 0));
+        url_decode(q_field_d, q_field);
+        SQL_MODE = 2;
+    }
 
-	  //fill out result string
-	  std::string resString = "";
-	  if (SQL_MODE == 1){
-	  	SqlEngine sqlEngine;
-	  	resString = sqlEngine.HandleRequest(qField, 1);
-	  }
-	  else if (SQL_MODE == 2){
-	  	SqlEngine sqlEngine;
-	  	resString = sqlEngine.HandleRequest(qField, 2);
-	  }
+    // Fill out result string.
+    std::string res_string = "";
+    SqlEngine sql_engine;
+    if (SQL_MODE == 1){
+        res_string = sql_engine.HandleRequest(q_field, 1);
+    }
+    else if (SQL_MODE == 2){
+        res_string = sql_engine.HandleRequest(q_field, 2);
+    }
 
-	// Open the file to send back.
-	  std::string full_path = "deploy/" + root_ + "/sqlpage.html";
-	  std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
-	  if (!is) {
-	    response->SetStatus(Response::NOT_FOUND);
-	    return RequestHandler::NOT_FOUND;
-	  }
+    // Open the file to send back.
+    std::string full_path = root_ + "/sqlpage.html";
+    std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
+    if (!is) {
+        response->SetStatus(Response::NOT_FOUND);
+        return RequestHandler::NOT_FOUND;
+    }
 
-	  // Fill out the response to be sent to the client.
-	  response->SetStatus(Response::OK);
-	  char buf[8192];
-	  std::string file_content = "";
-	  while (is.read(buf, sizeof(buf)).gcount() > 0) {
-	    file_content.append(buf, is.gcount());
-	  }
-	  if (resString.length() != 0){
-	  	file_content.append("<h1>Results:</h1>");
-	  }
-	  file_content.append(resString.c_str(), resString.length());
-	  std::string ender = "</body></html>";
-	  file_content.append(ender.c_str(), ender.length());
-	  response->SetBody(file_content);
-	  response->AddHeader("Content-Length", boost::lexical_cast<std::string>(file_content.length()));
-	  response->AddHeader("Content-Type", mime_types::extension_to_type("html"));
-	  return RequestHandler::OK;
+    // Fill out the response to be sent to the client.
+    response->SetStatus(Response::OK);
+    char buf[512];
+    std::string file_content = "";
+    while (is.read(buf, sizeof(buf)).gcount() > 0) {
+        file_content.append(buf, is.gcount());
+    }
+    if (res_string.length() != 0){
+        file_content.append("<h2>Results:</h2>");
+    }
+    file_content.append(res_string.c_str(), res_string.length());
+    std::string ender = "</div></body></html>";
+    file_content.append(ender.c_str(), ender.length());
+    response->SetBody(file_content);
+    response->AddHeader("Content-Length", boost::lexical_cast<std::string>(file_content.length()));
+    response->AddHeader("Content-Type", mime_types::extension_to_type("html"));
+    return RequestHandler::OK;
 }
 
 bool SqlHandler::url_decode(const std::string& in, std::string& out) {
-  out.clear();
-  out.reserve(in.size());
-  for (std::size_t i = 0; i < in.size(); ++i) {
-    if (in[i] == '%') {
-      if (i + 3 <= in.size()) {
-        int value = 0;
-        std::istringstream is(in.substr(i + 1, 2));
-        if (is >> std::hex >> value) {
-          out += static_cast<char>(value);
-          i += 2;
+    out.clear();
+    out.reserve(in.size());
+    for (std::size_t i = 0; i < in.size(); ++i) {
+        if (in[i] == '%') {
+            if (i + 3 <= in.size()) {
+                int value = 0;
+                std::istringstream is(in.substr(i + 1, 2));
+                if (is >> std::hex >> value) {
+                    out += static_cast<char>(value);
+                    i += 2;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else if (in[i] == '+') {
+            out += ' ';
         }
         else {
-          return false;
+            out += in[i];
         }
-      }
-      else {
-        return false;
-      }
     }
-    else if (in[i] == '+') {
-      out += ' ';
-    }
-    else {
-      out += in[i];
-    }
-  }
-  return true;
+    return true;
 }
 
 REGISTER_REQUEST_HANDLER(SqlHandler);
--- src/sql_engine.cpp
+++ src/sql_engine.cpp
@@ -1,19 +1,20 @@
+//
+// sql_engine.cpp
+// ~~~~~~~~~~~~~~~~~~~
+//
+
 #include <iostream>
 #include "sql_engine.hpp"
-
 #include <stdlib.h>
 #include <string>
 #include "mysql_connection.h"
-
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <resultset_metadata.h>
-
  
-std::string SqlEngine::HandleRequest(std::string& field, int mode)
-{
+std::string SqlEngine::HandleRequest(std::string& field, int mode) {
   std::string outString = "";
   try {
     sql::Driver *driver;
@@ -21,40 +22,44 @@ std::string SqlEngine::HandleRequest(std::string& field, int mode)
     sql::Statement *stmt;
     sql::ResultSet *res;
 
-    /* Create a connection */
+    // Create a connection.
     driver = get_driver_instance();
     con = driver->connect("54.190.63.110:6603", "root", "");
-    /* Connect to the MySQL test database */
+    // Connect to the MySQL database
     con->setSchema("DB");
 
-    //If its a query, we want to return some results
+    // If it's a query, we want to return some results.
     std::cout << "QueryStatement: " << field << "\n"; 
     stmt = con->createStatement();
-    if (mode == 1){   //query mode
+    // Query mode.
+    if (mode == 1) {  
       res = stmt->executeQuery(field);
       //get result set metadata
       sql::ResultSetMetaData *res_meta = res -> getMetaData();
       int columns = res_meta -> getColumnCount();
 
-      //Loop for each row
+      // Loop for each row.
       while (res->next()) {
-        /* Access column data by index, 1-indexed*/
+        // Access column data by index, 1-indexed
         for (int i = 1; i <= columns; i++) {
           outString += res->getString(i) + ", ";
         }
       }
-      delete res;
-    }
-    //if its an update, we dont need to return any results
-    else if (mode == 2){  //update mode
-      stmt->execute(field);
-      outString = "Update Success!";
     }
 
+    // If it's an update, we don't need to return any results
+    // Update mode.
+    else if (mode == 2) {
+      res = stmt->executeQuery(field);
+      outString = "Update Attempted.";
+    }
+
+    delete res;
     delete stmt;
     delete con;
 
-  } catch (sql::SQLException &e) {
+  }
+  catch (sql::SQLException &e) {
     outString = "MYSQL ERR: INVALID SYNTAX";
   }
 
